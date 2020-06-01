049ef59b00150f6a1a5056276466c467aaddaa68
michalbaryzewski <michalbaryz@gmail.com>
2020-06-01 13:03:19 +0200

    first commit, configuration completed

---
+++ src/main/java/com/cwm/carworkshop/CarWorkshopManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cwm.carworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWorkshopManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWorkshopManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwm/carworkshop/CarWorkshopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.carworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWorkshopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
